f1ec12cdb630c6cfb47da28b5c85e1baa0874686
Sneha Poulose K <sneha.poulose@rapidvaluesolutions.com>
2021-12-14 09:14:00 +0530

    -- removed unwanted variables .

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 var msec = 0,
   sec = 0,
   min = 0,
-  interval = 0
-  timerRunning = false;
-  let count = 0;
-var msecVar, secVar, minVar, hourVar;
+  interval = 0;
+timerRunning = false;
+count = 0;
 
 function setMSec() {
   count++;
-  min = Math.floor((count/100)/60);
-  sec = Math.floor((count/100) - (min*60));
-  msec =Math.floor(count - (sec * 100) - (min*6000));
-  document.getElementById("min").innerHTML = loadZero(min) ;
+  min = Math.floor(count / 100 / 60);
+  sec = Math.floor(count / 100 - min * 60);
+  msec = Math.floor(count - sec * 100 - min * 6000);
+  document.getElementById("min").innerHTML = loadZero(min);
   document.getElementById("sec").innerHTML = loadZero(sec);
   document.getElementById("msec").innerHTML = loadZero(msec);
-  
 }
 
 function startButton() {
-if(!timerRunning){
- interval= setInterval(setMSec,10)
- timerRunning = true;
-}
+  if (!timerRunning) {
+    interval = setInterval(setMSec, 10);
+    timerRunning = true;
+  }
 }
 
 function stopButton() {
@@ -29,52 +27,9 @@ function stopButton() {
   timerOn = 0;
 }
 function loadZero(time) {
-  if(time <=9){
+  if (time <= 9) {
     return "0" + time;
-  }
-  else{
+  } else {
     return time;
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
